7f15cec8951618843019a1333e50e900b43b2d6b
Marian Dziubiak <marian.dziubiak@gmail.com>
2019-05-16 17:27:54 +0200

    Add simple expression parser and syntax tree

--- src/Parser.hs
+++ src/Parser.hs
@@ -6,7 +6,10 @@ import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 
+import SyntaxTree
+
 type Parser = Parsec String String
+type CtxParser d = Parser (d SourcePos)
 
 spaceConsumer :: Parser ()
 spaceConsumer = L.space space1 empty blockCmt where
@@ -23,3 +26,84 @@ decimal = lexeme L.decimal
 
 integer :: Integral a => Parser a
 integer = L.signed spaceConsumer decimal
+
+double :: Parser Double
+double = L.signed spaceConsumer L.float
+
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+reserved :: String -> Parser ()
+reserved w = (lexeme . try) (string w *> notFollowedBy alphaNumChar)
+
+reservedWords = ["let", "in", "fun"]
+
+identifier :: Parser String
+identifier = (lexeme . try) (p >>= check)
+  where
+    p       = (:) <$> letterChar <*> many alphaNumChar
+    check x = if x `elem` reservedWords
+                then fail $ "keyword " ++ show x ++ " cannot be an identifier"
+                else return x
+
+withPosition :: Parser (SourcePos -> a) -> Parser a
+withPosition p = do
+    pos <- getSourcePos
+    f <- p
+    return (f pos)
+
+pDeclaration :: CtxParser LetDeclaration
+pDeclaration = pDLet
+
+pDLet :: CtxParser LetDeclaration
+pDLet = withPosition $ do
+    reserved "let"
+    s <- identifier
+    symbol "="
+    e <- pExpr
+    return (DLet s e)
+
+pExpr :: CtxParser Expr
+pExpr = pEApp
+
+pExprSimpl :: CtxParser Expr
+pExprSimpl = pELit <|> pEVar <|> pELet <|> pELambda <|> parens pExpr
+
+pELit :: CtxParser Expr
+pELit = withPosition (ELit <$> pLit)
+
+pEVar :: CtxParser Expr
+pEVar = withPosition (EVar <$> identifier)
+
+pELet :: CtxParser Expr
+pELet = withPosition $ do
+    d <- pDLet
+    reserved "in"
+    e <- pExpr
+    return (ELet d e)
+
+many2 p = do
+    a1 <- p
+    a2 <- p
+    many p >>= \l -> return $ a1 : a2 : l
+
+pEApp :: CtxParser Expr
+pEApp = try (foldl1 (\l r -> EApp l r (context l)) <$> (many2 pExprSimpl)) <|> pExprSimpl
+
+pELambda :: CtxParser Expr
+pELambda = withPosition $ do
+    reserved "fun"
+    x <- identifier
+    symbol "->"
+    e <- pExpr
+    return (ELambda x e)
+
+pLit :: CtxParser Literal
+pLit = withPosition (try pLInt <|> pLDouble <|> pLChar <|> pLString <|> pLUnit <|> pLBool)
+
+pLInt = LInt <$> integer
+pLDouble = LDouble <$> double
+pLChar = between (char '\'') (char '\'') (LChar <$> L.charLiteral)
+pLString = LString <$> (char '\"' >> manyTill L.charLiteral (char '\"'))
+pLUnit = symbol "()" >> return LUnit
+pLBool = LBool <$> try ((reserved "true" >> return True) <|> (reserved "false" >> return False))
---
+++ src/SyntaxTree.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE GADTs, DeriveFunctor #-}
+module SyntaxTree where
+
+class Contextual t where
+  context :: t a -> a
+
+data Literal a =
+      LInt    Int     a
+    | LDouble Double  a
+    | LChar   Char    a
+    | LString String  a
+    | LUnit           a
+    | LBool   Bool    a
+  deriving (Eq, Show, Functor)
+
+data Expr a =
+      ELit    (Literal a)  a
+    | EVar    String  a
+    | ELet    (LetDeclaration a) (Expr a)  a
+    | EApp    (Expr a) (Expr a)  a
+    | ELambda String (Expr a)  a
+  deriving (Eq, Show, Functor)
+
+data LetDeclaration a =
+      DLet    String (Expr a) a
+  deriving (Eq, Show, Functor)
+
+instance Contextual Literal where
+  context (LInt _ c) = c
+  context (LDouble _ c) = c
+  context (LChar _ c) = c
+  context (LString _ c) = c
+  context (LUnit c) = c
+  context (LBool _ c) = c
+
+instance Contextual Expr where
+  context (ELit _ c) = c
+  context (EVar _ c) = c
+  context (ELet _ _ c) = c
+  context (EApp _ _ c) = c
+  context (ELambda _ _ c) = c
+
+instance Contextual LetDeclaration where
+  context (DLet _ _ c) = c

62f5647e83a444238b22c2e915d21564c2c992c4
Marian Dziubiak <marian.dziubiak@gmail.com>
2019-06-17 22:15:10 +0200

    Finish the basic data cases for deriving Demote and Single

--- src/Basic/TH.hs
+++ src/Basic/TH.hs
@@ -24,8 +24,14 @@ deriveDemote ty = do
     (tyConName, tyVars, constructors) <- case tyCon of
         DataD _ name vars _ cs _ -> return (name, vars, cs)
         NewtypeD _ name vars _ c _ -> return (name, vars, [c])
-        _ -> fail "genSingl: tyCon may not be a type synonym."
-    demote <- tySynInstD ''Demote (tySynEqn [conT tyConName] (conT tyConName))
+        _ -> fail "deriveDemote: tyCon may not be a type synonym."
+    let appliedDemote = foldl1 AppT appElements
+        appElements = (ConT tyConName) : map ((ConT ''Demote `AppT`) . tvToT) tyVars
+        tvToT = \x -> case x of
+            PlainTV n -> VarT n
+            KindedTV n _ -> VarT n
+        appliedRaw = foldl1 appT (conT tyConName : map (return . tvToT) tyVars)
+    demote <- tySynInstD ''Demote (tySynEqn [appliedRaw] (return appliedDemote))
     return [demote]
 deriveSingl :: Name -> Q [Dec]
 deriveSingl ty = do
@@ -33,69 +39,79 @@ deriveSingl ty = do
     (tyConName, tyVars, constructors) <- case tyCon of
         DataD _ name vars _ cs _ -> return (name, vars, cs)
         NewtypeD _ name vars _ c _ -> return (name, vars, [c])
-        _ -> fail "genSingl: tyCon may not be a type synonym."
-    let instanceType = conT ''Single `appT`
-            (foldl apply (conT tyConName) tyVars)
+        _ -> fail "deriveSingl: tyCon may not be a type synonym."
+    let instanceType = conT ''Single `appT` appliedType
+        appliedType = foldl apply (conT tyConName) tyVars
+        preds = sequence $ map (conT ''Single `apply`) tyVars
     
-    sing <- genSingl tyConName tyVars constructors
-    inst <- instanceD (return []) instanceType (map return sing)
+    sing <- genSingl appliedType tyConName tyVars constructors
+    inst <- instanceD (preds) instanceType (map return sing)
     return [inst]
   where
     apply t (PlainTV name)    = appT t (varT name)
     apply t (KindedTV name _) = appT t (varT name)
 
-genSingl :: Name -> [TyVarBndr] -> [Con] -> Q [Dec]
-genSingl t vars cs = do
-    let kind = AppT (ConT ''Demote) (ConT t)
+genSingl :: TypeQ -> Name -> [TyVarBndr] -> [Con] -> Q [Dec]
+genSingl tpq t vars cs = do
+    appType <- tpq
+    let kind = AppT (ConT ''Demote) appType
         vbtToBt (a,b,c) = (b,c)
-        getUnqualifiedName (Name (OccName s) _) = last $ split '.' s
-        mkCon n bts =
-            let nn = mkName ("S" ++ getUnqualifiedName n) in
-            GadtC [nn] {-TODO-}bts (ConT ''Singl `AppT` PromotedT n)
-        mapper = \c -> case c of
-            RecC n bts -> mkCon n (map vbtToBt bts)
-            GadtC [n] bts t -> mkCon n bts
-            NormalC n bts -> mkCon n bts
+        mkCon kvs n bts = do
+            let nn = mkSname n
+            (nbts, namedKinds) <- unzip <$> mapM convertArgument bts
+            let names = map fst namedKinds
+            return $ ForallC (kvs ++ map kindedProper namedKinds) []
+                $ GadtC [nn] nbts (ConT ''Singl `AppT` (foldl AppT (PromotedT n) (map VarT names)))
+        mkBangType t = (Bang NoSourceUnpackedness NoSourceStrictness, t)
+        convertArgument (_,t) = do
+            name <- newName "q"
+            tp <- conT ''Singl `appT` (varT name)
+            return (mkBangType tp, (name, (AppT (ConT ''Demote) t))) 
+        kindedProper (n, t) = KindedTV n t
+        mapper kvs = \c -> case c of
+            RecC n bts -> mkCon kvs n (map vbtToBt bts)
+            GadtC [n] bts t -> mkCon kvs n bts
+            NormalC n bts -> mkCon kvs n bts
             _ -> undefined
-        newCs = map (return . mapper) cs
+        newCs = map (mapper vars) cs
+    newCsu <- sequence newCs
+    let pcs = zip cs newCsu
     sequence [
-        dataInstD (return []) ''Singl [return $ SigT (VarT $ mkName "a") kind] Nothing newCs [],
-        funD 'fromSingl [genFromClause ()],--(map genFromClause cs),
-        funD 'toSingl [genFromClause ()]--(map genToClause cs)
+        dataInstD (return []) ''Singl [return $ SigT (VarT $ mkName "s") kind] Nothing newCs [],
+        funD 'fromSingl (map genFromClause newCsu),
+        funD 'toSingl (map genToClause cs)
      ]
     where
-        genFromClause c = return $ Clause [WildP] (NormalB (VarE (mkName "undefined"))) []
-        genToClause c = return $ Clause [WildP] (NormalB (VarE (mkName "undefined"))) []
--- genFmapClause :: Con -> Q Clause
--- genFmapClause c@(NormalC name fieldTypes)
---    = do f          <- newName "f"
---         fieldNames <- replicateM (length fieldTypes) (newName "x")
-
---         let pats = varP f:[conP name (map varP fieldNames)]
---             body = normalB $ appsE $
---                 conE name : map (newField f) (zip fieldNames fieldTypes)
-
---         clause pats body []
---     where
---         (>>=) = Prelude.(>>=)
-
--- newField :: Name -> (Name, StrictType) -> Q Exp
--- newField f (x, (_, fieldType))
---     = do 
---         Just (Deriving typeCon typeVar) <- getQ
---         case fieldType of
---             VarT typeVar' | typeVar' == typeVar ->
---                 [| $(varE f) $(varE x) |]
---             ty `AppT` VarT typeVar' |
---                 leftmost ty == (ConT typeCon) && typeVar' == typeVar ->
---                 [| fmap $(varE f) $(varE x) |]
---             _ -> [| $(varE x) |]
---     where
---         (>>=) = Prelude.(>>=)
-
--- leftmost :: Type -> Type
--- leftmost (AppT ty1 _) = leftmost ty1
--- leftmost ty           = ty
+        getUnqualifiedName (Name (OccName s) _) = last $ split '.' s
+        argToPat (_,t) = do
+            a <- newName "a"
+            return (varP a, a)
+        stripSname = mkName . tail . getUnqualifiedName
+        mkSname n = mkName ("S" ++ getUnqualifiedName n)
+        genFromClause c = case c of
+            ForallC ks _ (GadtC [name] args _) -> do
+                (pats, names) <- unzip <$> mapM argToPat args
+                p <- conP name pats
+                return $ Clause [p] (NormalB (foldl AppE (ConE $ stripSname name) (map ((VarE 'fromSingl `AppE`) . VarE) names))) []
+            _ -> undefined
+        genToClause c = do
+            (name, pats, names) <- case c of
+                RecC n vbts -> do
+                    (pats, names) <- unzip <$> mapM (\(a,b,c) -> argToPat (b,c)) vbts
+                    return (n, pats, names)
+                GadtC [n] bts _ -> do
+                    (pats, names) <- unzip <$> mapM argToPat bts
+                    return (n, pats, names)
+                NormalC n bts -> do
+                    (pats, names) <- unzip <$> mapM argToPat bts
+                    return (n, pats, names)
+                _ -> undefined
+            p <- conP name pats
+            return $ Clause [p] (NormalB 
+                (CaseE 
+                    (TupE (map ((VarE 'toSingl `AppE`) . VarE) names)) 
+                    [Match (TupP (map (\n -> ConP 'SomeSingl [VarP n]) names)) (NormalB (ConE 'SomeSingl `AppE` foldl AppE (ConE $ mkSname name) (map VarE names))) []]
+                    )) []
 
 split :: Char -> String -> [String]
 split c s = case rest of
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,28 @@
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE GADTs, TemplateHaskell, KindSignatures, TypeFamilies, DataKinds, TypeFamilyDependencies, FlexibleInstances, PolyKinds, QuasiQuotes #-}
 
 import Parser
 import Text.Megaparsec
 import Parser
 import TypeChecking.TC
 import Basic.TH
+import Basic.Single
 import System.Exit
 import Str
+
+data List a = Nila | Consa a (List a)
+    deriving Show
+
+$(deriveDemote ''Maybe)
+$(deriveSingl ''Maybe)
+
+$(deriveDemote ''List)
+$(deriveSingl ''List)
+
+test1 = SJust (SConsa STrue SNila)
+test2 = fromSingl test1
+test3 = toSingl test2
+
+
 
 -- TODO add some tests
 shouldPass :: [(String, String)]
@@ -38,7 +54,6 @@ oneFail name PTypeChecker code = case runParsing name code of
         Left err -> return ()
         Right _ -> putStrLn ("Should fail in TC, but haven't: " ++ name) >> exitWith (ExitFailure 5)
 
-
 onePass :: String -> String -> IO ()
 onePass name code = case runParsing name code of
     Left err -> putStrLn err >> exitWith (ExitFailure 2)
@@ -50,5 +65,8 @@ uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
 uncurry3 f (x,y,z) = f x y z
 
 main :: IO ()
-main = mapM_ (uncurry onePass) shouldPass >>
+main = do
+    mapM_ (uncurry onePass) shouldPass
     mapM_ (uncurry3 oneFail) shouldn'tPass
+    putStrLn "\nTH derivation test:"
+    print test2
